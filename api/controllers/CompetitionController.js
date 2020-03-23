a3bc967e3a9fd313a8234e468757b366986f5731
Becky543 <17212081@life.hkbu.edu.hk>
2020-03-23 15:42:17 +0800

    Added update action

--- api/controllers/CompetitionController.js
+++ api/controllers/CompetitionController.js
@@ -30,14 +30,14 @@ module.exports = {
     },
 
 
-        // action - start
-        start: async function (req, res) {
-            var model = await Competition.findOne(req.params.id);
-    
-            model.startTime = new Date();
-            await Competition.update(req.params.id).set(model);
-            return res.redirect('/competition/admin');
-        },
+    // action - start
+    start: async function (req, res) {
+        var model = await Competition.findOne(req.params.id);
+
+        model.startTime = new Date();
+        await Competition.update(req.params.id).set(model);
+        return res.redirect('/competition/admin');
+    },
 
     // action - admin
     admin: async function (req, res) {
@@ -105,6 +105,53 @@ module.exports = {
 
     },
 
+    // action - 
+    update: async function (req, res) {
+
+        if (req.method == "GET") {
+
+            var model = await Competition.findOne(req.params.id);
+
+            if (!model) return res.notFound();
+
+            return res.view('competition/update', { competition: model });
+
+        } else {
+
+            if (!req.body.Competition)
+                return res.badRequest("Form-data not received.");
+
+            var models = await Competition.update(req.params.id).set({
+                athleteName: req.body.Competition.athleteName,
+                athleteID: req.body.Competition.athleteID,
+                competitionEvent: req.body.Competition.competitionEvent,
+
+                e1Score: req.body.Competition.e1Score,
+                e2Score: req.body.Competition.e2Score,
+                e3Score: req.body.Competition.e3Score,
+                e4Score: req.body.Competition.e4Score,
+                e5Score: req.body.Competition.e5Score,
+                d1Score: req.body.Competition.d1Score,
+                e2Score: req.body.Competition.e2Score,
+
+                dAvgScore: req.body.Competition.dAvgScore,
+                eAvgScore: req.body.Competition.eAvgScore,
+                totalScore: req.body.Competition.totalScore,
+             
+                startTime: req.body.Competition.startTime,
+                endTime: req.body.Competition.endTime,
+
+                //createdDate: req.body.Estate.createdDate,
+                //updatedDate: new Date().toLocaleDateString(),
+            }).fetch();
+
+            if (models.length == 0) return res.notFound();
+
+            //return res.ok("Record updated");
+            return res.redirect("/estate/admin/");
+
+        }
+    },
 
     // action - import excel file
     import_xlsx: async function (req, res) {
