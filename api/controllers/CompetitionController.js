1b4156e12dd7c17723f4143904db61256e8e3d3d
Becky543 <17212081@life.hkbu.edu.hk>
2020-04-29 09:25:24 +0800

    .

--- api/controllers/CompetitionController.js
+++ api/controllers/CompetitionController.js
@@ -397,16 +397,12 @@ module.exports = {
             if (models.length == 0) return res.notFound();
 
             return res.ok("Pending for approval of D Judge...");
-            //return res.redirect('/competition/updateE1/6');  // hardcode url id
+            // return res.redirect('/competition/waitingE1');
+            
+            
+            
 
-            //return res.ok("Record updated");
-            //req.params.id = req.params.id + 1
-
-
-            //res.render('/blogs/' + blog._id, {blog_id: req.params.id});
-            //competition = req.body.Competition
-            //return res.render('//competition/updateE1/' + competition.id, {competition_id: req.params.id});
-            //return res.redirect('/competition/updateE1/%d', req.params.id+1);
+  
 
         }
     },
